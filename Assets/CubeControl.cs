117c05be5cb3676bb978a037a0538c67038fd70c
H3RURG <vladimir926226@gmail.com>
2020-02-09 12:58:42 +0300

    release

---
+++ Assets/CubeControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour
+{
+
+    public Color[] cubeColors;
+
+    private MeshRenderer meshRenderer;
+    private int randomColor;
+    private float speed;
+
+    private void Awake()
+    {
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        meshRenderer.material.color = cubeColors[Random.Range (0, cubeColors.Length)];
+
+        randomColor = Random.Range(0, cubeColors.Length);
+        speed = Random.Range(2, 5);
+    }
+
+    private void Update()
+    {
+        ChangeColor();
+    }
+
+    private void ChangeColor ()
+    {
+        meshRenderer.material.color = Color.Lerp(meshRenderer.material.color, cubeColors[randomColor], Time.deltaTime * speed);
+
+        if (meshRenderer.material.color == cubeColors[randomColor])
+            randomColor = Random.Range(0, cubeColors.Length);
+    }
+}
